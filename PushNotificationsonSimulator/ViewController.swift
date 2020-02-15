e9e7be8f1cb9a5819a3a3e94732db8cba91fd1f3
Shigasuresh@123 <shigasuresh546@gmail.com>
2020-02-16 00:28:59 +0530

    Initial Commit

---
+++ PushNotificationsonSimulator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PushNotificationsonSimulator
+//
+//  Created by shiga on 16/02/20.
+//  Copyright © 2020 shiga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
